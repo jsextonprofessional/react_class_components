61455026eab037d034de98f4b72a46b21cf0b0b8
jsprofessional <jshuman@protonmail.com>
2021-09-09 16:31:42 -0500

    Added plat_synth

---
+++ plat_synthetic_events/synth/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
