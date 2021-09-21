add092e77a09b74245a2f997239769d0b2aa6a5f
jsprofessional <jshuman@protonmail.com>
2021-09-21 17:45:58 -0500

    Started react_blocks

---
+++ React_Blocks/react_blocks/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component{    
+    render(){
+        return(
+            <div>
+                <h1>YEET -- GREEN</h1>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
